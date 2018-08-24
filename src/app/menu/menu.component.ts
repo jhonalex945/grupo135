062a218100a9afdc4a34c017a55c097bac62c164
jhonalex945@hotmail.com <jhonalex945@hotmail.com>
2018-08-24 17:03:11 -0500

    Cambio en menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -12,7 +12,7 @@ export class MenuComponent implements OnInit {
 
   constructor()
     {
-      this.menu = ['Inicio', 'Acerca', 'Ayuda', 'Clientes']
+      this.menu = ['Inicio1', 'Acerca', 'Ayuda', 'Clientes']
       this.menuInicio = ['Ingresar', 'Salir']
     }
 
